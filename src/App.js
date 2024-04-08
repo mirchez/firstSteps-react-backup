833b7f25c7ac1c48ef7c7f8341757d4723af5848
Miguel <mmirandasanchez16@gmail.com>
2024-04-08 08:10:45 -0400

    copia de seguridad

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MiComponente from './MiComponente';
+import { SegundoComponente } from './SegundoComponente';
+import { TercerComponente } from './TercerComponente';
+import { EventosComponentes } from "./EventosComponentes";
+
+function App() {
+
+  const ficha_med = {
+    altura : "187 cm",
+    grupo : "H+",
+    estado : "Bueno",
+    alergias : "Ninguna"
+  };
