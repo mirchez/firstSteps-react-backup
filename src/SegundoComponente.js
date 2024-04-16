983104c3decaf284f6043a446c97b90d6d89fc5f
Miguel <mmirandasanchez16@gmail.com>
2024-04-16 19:25:38 -0400

    copia de seguridad

--- src/SegundoComponente.js
+++ src/SegundoComponente.js
@@ -1,1 +1,23 @@
+import React from 'react'
 
+export const SegundoComponente = () => {
+
+const libros = ["Harry Potter", "Juego de Tronos", "Clean Code"];
+
+
+    return (
+        <div className='segundo-componente'>
+
+            <h1>Listado de Libros</h1>
+            {libros.length > 0 ? 
+            (<ul>{
+                    libros.map((libro,indice) => {
+                        return <li key={indice}>{libro}</li>;
+                    })
+                }   
+            </ul>)
+            :(<p>No hay libros</p>)
+        }
+        </div>
+    )
+}
